2020beb50a20468e365a187caebfca004ca3b646
Paul Mansfield <paul.mansfield@nielsen.com>
2023-04-14 19:28:08 +1000

    3.333 changes

--- compare.py
+++ compare.py
@@ -2,22 +2,23 @@ import cv2
 import numpy as np
 from pathlib import Path
 from time import time
+import datetime
 
 # Open the 23.976 fps video
-cap1 = cv2.VideoCapture(str(Path('~').expanduser())+'/Downloads/Evangelion - 3.0 You Can (Not) Redo (2012)/Evangelion - 3.0 You Can (Not) Redo (2012).mkv')
+cap1 = cv2.VideoCapture(str(Path('~').expanduser())+'/Documents/Evangelion - 3.33 You Can (Not) Redo (2012) CUSTOM MULTi 1080p 10bits BluRay x265 EAC3 AC3 5.1 v3 -Punisher694.mkv')
 
 # Open the 24 fps video
-cap2 = cv2.VideoCapture(str(Path('~').expanduser())+'/Downloads/output.mp4')
+cap2 = cv2.VideoCapture(str(Path('~').expanduser())+'/Documents/Evangelion - 3.333 You Can (Not) Redo (2012) CUSTOM MULTi 1080p 10bits BluRay x265 DDP 5.1 v2 -Punisher694.mkv')
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v') # codec
-fps = 24
+target_fps = 23.976023976023978
 frameSize = (3840,1632)
-out = cv2.VideoWriter('output3.mp4', fourcc, fps, frameSize)
+out = cv2.VideoWriter('output3.mp4', fourcc, target_fps, frameSize)
 # Get the original framerates
 orig_fps1 = cap1.get(cv2.CAP_PROP_FPS)
 orig_fps2 = cap2.get(cv2.CAP_PROP_FPS)
 # Define the target framerate
-target_fps = 23.976023976023978
+
 print(orig_fps1)
 print(orig_fps2)
 # Calculate the frame duration for the target framerate
@@ -27,9 +28,9 @@ target_frame_duration = 1 / target_fps
 
 
 x_seconds = 0
-cap1.set(cv2.CAP_PROP_POS_FRAMES, 392)
+cap1.set(cv2.CAP_PROP_POS_FRAMES, 0)
 x_seconds = 0
-cap2.set(cv2.CAP_PROP_POS_FRAMES, 0)
+cap2.set(cv2.CAP_PROP_POS_FRAMES, 24)
 
 frame_counter1 = 1
 prev_frame_time1 = 0
@@ -38,6 +39,9 @@ frame_counter2 = cap2.get(cv2.CAP_PROP_POS_FRAMES)
 prev_frame_time2 = 0
 
 cv2.namedWindow('video', cv2.WINDOW_NORMAL)
+
+f = open('values.txt','w')
+f.write(f'time,mseg,msec,msegb\n')
 
 # Loop through each frame of the videos
 while True:
@@ -54,24 +58,24 @@ while True:
         break
     
     # If the first video has a different framerate, resample it
-    # if orig_fps1 != target_fps:
-    #     curr_frame_time1 = frame_counter1 * orig_fps1
-    #     if curr_frame_time1 - prev_frame_time1 >= target_frame_duration:
-    #         # Write the resampled frame to the output
-    #         # Note that we need to use interpolation when resizing to prevent artifacts
-    #         prev_frame_time1 = curr_frame_time1
-    #     else:
-    #         continue
+    if orig_fps1 != target_fps:
+        curr_frame_time1 = frame_counter1 * orig_fps1
+        if curr_frame_time1 - prev_frame_time1 >= target_frame_duration:
+            # Write the resampled frame to the output
+            # Note that we need to use interpolation when resizing to prevent artifacts
+            prev_frame_time1 = curr_frame_time1
+        else:
+            continue
 
-    # # If the second video has a different framerate, resample it
-    # if orig_fps2 != target_fps:
-    #     curr_frame_time2 = frame_counter2 * orig_fps2
-    #     if curr_frame_time2 - prev_frame_time2 >= target_frame_duration:
-    #         # Write the resampled frame to the output
-    #         # Note that we need to use interpolation when resizing to prevent artifacts
-    #         prev_frame_time2 = curr_frame_time2
-    #     else:
-    #         continue
+    # If the second video has a different framerate, resample it
+    if orig_fps2 != target_fps:
+        curr_frame_time2 = frame_counter2 * orig_fps2
+        if curr_frame_time2 - prev_frame_time2 >= target_frame_duration:
+            # Write the resampled frame to the output
+            # Note that we need to use interpolation when resizing to prevent artifacts
+            prev_frame_time2 = curr_frame_time2
+        else:
+            continue
     
    
     # Increment the frame counters for each video
@@ -81,31 +85,31 @@ while True:
     frame1g = cv2.cvtColor(frame1, cv2.COLOR_RGB2GRAY)
     frame2g = cv2.cvtColor(frame2, cv2.COLOR_RGB2GRAY)
 
-   
     avg_luma = frame1.mean()
     avg_luma2 = frame2.mean()
     luma_diff =  (avg_luma - avg_luma2)
     #print(f'lumadiff:{luma_diff}')
     frame2g = cv2.add(frame2g,luma_diff)
-    #diff_img = cv2.GaussianBlur(frame1g, (15, 15), 0)
-    #diff_img2 = cv2.GaussianBlur(frame2g, (15, 15), 0)
+    diff_img = cv2.GaussianBlur(frame1g, (15, 15), 0)
+    diff_img2 = cv2.GaussianBlur(frame2g, (15, 15), 0)
 
     diff = cv2.absdiff( frame1g, frame2g)
     diff2 = cv2.absdiff( frame1, frame2)
+    diff3 = cv2.absdiff(diff_img,diff_img2)
     mse = np.mean(diff ** 2)
+    mse2 = np.mean(diff2 ** 2)
+    mse3 = np.mean(diff3 ** 2)
+    f.write(f'{cap1.get(cv2.CAP_PROP_POS_MSEC)/1000},{mse},{mse2},{mse3}\n')
     #mse = (cv2.sumElems(diff)[0] / (diff.shape[0] * diff.shape[1])) ** 2
     #print(mse)
-   
     # Display the difference between the frames
-    cv2.putText(frame1, f'3.33: {cap1.get(cv2.CAP_PROP_POS_MSEC)/1000}',(10,50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    cv2.putText(frame2, f'3.3333 BD: {cap2.get(cv2.CAP_PROP_POS_MSEC)/1000}',(10,50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     layout = cv2.hconcat([frame1,frame2])
 
     diff = cv2.cvtColor(diff,cv2.COLOR_GRAY2BGR)
 
     #converted_layout2_clr = cv2.addWeighted(diff2,20,diff2,2,0.0)
-    cv2.putText(diff, f'{mse}',(10,50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    
     layout2 = cv2.hconcat([diff,diff2])
     #print(layout.shape)
     #print(layout2.shape)
@@ -115,15 +119,23 @@ while True:
     #cv2.imshow('f2g',frame2g)
     #print(layout3.shape)
     #k = cv2.waitKey(1) & 0xFF
-    print(mse)
-    if mse > 12.8:
-        cv2.imshow('video', layout3)
-        out.write(layout3)
+    layout3 = cv2.copyMakeBorder(layout3, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    cv2.putText(layout3, f'3.33 BD: {str(datetime.timedelta(seconds=cap1.get(cv2.CAP_PROP_POS_MSEC)//1000))}',(110,50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.putText(layout3, f'3.333 BD: {str(datetime.timedelta(seconds=cap2.get(cv2.CAP_PROP_POS_MSEC)//1000))}',(110+1920,50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.putText(layout3, f'mse: {mse}, msec: {mse2}, msegb: {mse3}',(110,160+816+816),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.imshow('video', layout3)
+    out.write(layout3)
     key = cv2.waitKey(1)
     if key == ord('a'):
         cap1.set(cv2.CAP_PROP_POS_MSEC, cap1.get(cv2.CAP_PROP_POS_MSEC)+20000)
         cap2.set(cv2.CAP_PROP_POS_MSEC, cap2.get(cv2.CAP_PROP_POS_MSEC)+20000)
+    if key == ord('d'):
+        cap1.set(cv2.CAP_PROP_POS_MSEC, cap1.get(cv2.CAP_PROP_POS_MSEC)-20000)
+        cap2.set(cv2.CAP_PROP_POS_MSEC, cap2.get(cv2.CAP_PROP_POS_MSEC)-20000)
+    if key == ord(' '):
+        cv2.waitKey(0)
 
 # Release the video capture objects
 cap1.release()
 cap2.release()
+f.close()
